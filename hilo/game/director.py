23dcd38446eee42ebad3bf0861f35ce7bde41979
VicGa <90812185+VicGa@users.noreply.github.com>
2022-06-17 12:05:31 -0300

    close to finished, the results are fixed

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -3,13 +3,11 @@ from hilo import Hil
 class Director:
 
     def __init__(self):
-        self.hilo = []
         self.is_playing = True
-        self.score = 0
+        self.score = 300
         self.total_score = 0
-        for i in range(1):
-            hil = Hil()
-            self.hilo.append(hil)
+        hil = Hil()
+        self.hilo = hil
         
 
     def start_game(self):
@@ -25,7 +23,7 @@ class Director:
 
     def get_inputs(self):
         """ ASK THE USER IF THE CARD IS HIGER OR LOWER"""
-        roll_hilo = input('Higer or lower? [h/l]')
+        roll_hilo = input('Higer or lower? [h/l] ')
         if roll_hilo == "h":
             self.is_playing = roll_hilo
         elif roll_hilo == "l":
@@ -35,29 +33,33 @@ class Director:
     def do_updates(self):
         
         if self.is_playing == "h":
-            for i in range (len(self.hilo)):
-                hilo = self.hilo[i]
-                hilo.roll_h()
-                self.score += hilo.points
-        if self.is_playing == "l":
-            for i in range (len(self.hilo)):
-                hilo = self.hilo[i]
-                hilo.roll_l()
-                self.score += hilo.points 
+            hilo = self.hilo
+            hilo.roll_h()
+            self.score = hilo.points
+            self.total_score = self.score
 
-        self.total_score += self.score
+
+
+        elif self.is_playing == "l":
+
+            hilo = self.hilo
+            hilo.roll_l()
+            self.score = hilo.points
+            self.total_score = self.score
+
 
     
     def do_outputs(self):
         values = ""
         val_next = ""
-        for i in range(len(self.hilo)):
-            hilo = self.hilo[i]
-            values += f"{hilo.value} "
-            val_next += f"{hilo.next} "
 
-        print(f"Your card is: {values}")
+        hilo = self.hilo
+        values = f"{hilo.value}"
+        print(f"The card is: {values}")
+
         self.get_inputs()
-        print(f"Your score is: {self.total_score}")
-        print (f"The next card was: {val_next}\n")
-        self.is_playing == (self.score > 0)
+        val_next = f"{hilo.next} "
+
+        print (f"Next Card Was: {val_next}")
+        print(f"Your score is: {self.total_score}\n")
+        #self.is_playing == (self.score > 0)
--- hilo/game/hilo.py
+++ hilo/game/hilo.py
@@ -5,22 +5,26 @@ class Hil:
         self.play = True
         self.value = 0
         self.next = 0
-        self.points = 0
+        
+        self.points =300
 
     def roll_h(self):
         self.value = random.randint(1,13)
         self.next = random.randint(1,13)
-        if self.value < self.next:
+        
+        if self.value <= self.next:
             self.points += 100
         else:
             self.points -= 75
     def roll_l(self):
         self.value = random.randint(1,13)
         self.next = random.randint(1,13)
-        if self.value > self.next:
+
+        if self.value >= self.next:
             self.points += 100
+        
         else:
             self.points -= 75
-    
+        
 
         
