5bb10303ab806edd14f3049e1a7952e3c6df57ad
VicGa <90812185+VicGa@users.noreply.github.com>
2022-06-18 22:26:30 -0300

    finished hilo game

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,9 +1,11 @@
+from re import T
 from hilo import Hil
 
 class Director:
 
     def __init__(self):
         self.is_playing = True
+        self.start =""
         self.score = 300
         self.total_score = 0
         hil = Hil()
@@ -17,49 +19,65 @@ class Director:
             self (Director): an instance of Director.
         """
 
-        while self.is_playing:
-            self.do_updates()
+        while self.is_playing and self.total_score >= 0:
             self.do_outputs()
 
     def get_inputs(self):
         """ ASK THE USER IF THE CARD IS HIGER OR LOWER"""
         roll_hilo = input('Higer or lower? [h/l] ')
-        if roll_hilo == "h":
-            self.is_playing = roll_hilo
+        self.start = roll_hilo
+    ''' if roll_hilo == "h":
+            self.start = roll_hilo
         elif roll_hilo == "l":
-            self.is_playing = roll_hilo
+            self.start = roll_hilo'''
 
 
     def do_updates(self):
         
-        if self.is_playing == "h":
+        if self.start == "h":
             hilo = self.hilo
             hilo.roll_h()
             self.score = hilo.points
-            self.total_score = self.score
 
+            
 
-
-        elif self.is_playing == "l":
+        elif self.start == "l":
 
             hilo = self.hilo
             hilo.roll_l()
             self.score = hilo.points
-            self.total_score = self.score
+
+        else:
+            self.is_playing = False
+        
+        self.total_score = self.score
 
 
     
     def do_outputs(self):
+
         values = ""
         val_next = ""
 
         hilo = self.hilo
-        values = f"{hilo.value}"
-        print(f"The card is: {values}")
 
-        self.get_inputs()
+        values = f"{hilo.value}"
         val_next = f"{hilo.next} "
+        self.do_updates()
+
+        print(f"The card is: {values}")
+        self.get_inputs()
 
         print (f"Next Card Was: {val_next}")
         print(f"Your score is: {self.total_score}\n")
+        ask = input("Play again? y/n\n")
+
+        if ask == "y":
+            self.is_playing = True
+        else:
+            self.is_playing = False
+        
+        
+
+
         #self.is_playing == (self.score > 0)
